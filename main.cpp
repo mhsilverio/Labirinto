c10a5de072beaa7873ad1a24804a5e07694980c2
Milena Silvério <83962009+mhsilverio@users.noreply.github.com>
2021-09-19 20:11:08 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,211 @@
+//MILENA HELOÍSA DE AMORIM SILVÉRIO
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Pilha.h"
+
+
+using namespace std;
+
+bool verificacao(char vef) {
+    return vef == 'S' || vef == ' ';
+}
+
+int main() {
+    vector<string> labirinto = {
+        "XXXXXXXXXXXXXXXXXXXXX",
+        "X     X     X     X X",
+        "XX XX XXXXX X X X   X",
+        "X   X       XXX XXX X",
+        "X X X XXXXXXX   X   X",
+        "X X   X   X X X   X X",
+        "X XXXXX X X   XXXXX X",
+        "X X     X XXX  X    X",
+        "X X X X X   XX XX XXX",
+         "X X XXX X X     X   X",
+        "E X X X X XXXXX XXX X",
+         "X     X X   X X X   X",
+        "X XXXXX XXX X XXX X X",
+        "X X       X X   X X X",
+        "X   XXX X X XXX X X X",
+        "XXX X X X X X X X XXX",
+        "X X   X X   X   X   X",
+        "X XXX XXXXXXXXX XXX X",
+        "X                 X X",
+        "XXXXXXXXXXXXXXXXXXXSX"
+    };
+
+    vector<string> labirinto_mostra = {
+        "XXXXXXXXXXXXXXXXXXXXX",
+        "X     X     X     X X",
+        "XX XX XXXXX X X X   X",
+        "X   X       XXX XXX X",
+        "X X X XXXXXXX   X   X",
+        "X X   X   X X X   X X",
+        "X XXXXX X X   XXXXX X",
+        "X X     X XXX  X    X",
+        "X X X X X   XX XX XXX",
+         "X X XXX X X     X   X",
+        "E X X X X XXXXX XXX X",
+         "X     X X   X X X   X",
+        "X XXXXX XXX X XXX X X",
+        "X X       X X   X X X",
+        "X   XXX X X XXX X X X",
+        "XXX X X X X X X X XXX",
+        "X X   X X   X   X   X",
+        "X XXX XXXXXXXXX XXX X",
+        "X                 X X",
+        "XXXXXXXXXXXXXXXXXXXSX"
+    };
+
+    int x = 10;
+    int y = 0;
+
+    Pilha px;
+    Pilha py;
+    int i = 0;
+
+    Pilha px_;
+    Pilha py_;
+    int i_ = 0;
+    int cont = 0;
+
+    /* LEGENDA:
+            x+1 = DESCE UMA CASA
+            x-1 = SOBE UMA CASA
+            y+1 = VAI PRA DIREITA UMA CAS
+            y-1 = VAI PRA ESQUERDA UMA CASA
+             *  = REPRESENTA O CAMINHO PERCORRIDO
+                    */
+
+    cout << "\n* * * LABIRINTO * * * " << endl;
+    cout << "  PEDA/2021 - PUCPR " << endl;
+    cout << "   Milena Silverio" << endl << endl;
+
+    cout << "Localizacao da E (Entrada): " << "( " << x << ", " << y << " )" << endl << endl;
+
+    while (labirinto[x][y] != 'S') {
+        if (verificacao(labirinto[x + 1][y])) {
+            ++x;
+            px.push(x);
+            py.push(y);
+            if (labirinto[x][y] != 'S') {
+                labirinto[x][y] = '*';
+                labirinto_mostra[x][y] = '*';
+            }
+            ++i;
+            ++i_;
+        }
+        else if (verificacao(labirinto[x - 1][y])) {
+            --x;
+            px.push(x);
+            py.push(y);
+            if (labirinto[x][y] != 'S') {
+                labirinto[x][y] = '*';
+                labirinto_mostra[x][y] = '*';
+            }
+            ++i;
+            ++i_;
+        }
+        else if (verificacao(labirinto[x][y + 1])) {
+            ++y;
+            px.push(x);
+            py.push(y);
+            if (labirinto[x][y] != 'S') {
+                labirinto[x][y] = '*';
+                labirinto_mostra[x][y] = '*';
+            }
+            ++i;
+            ++i_;
+        }
+        else if (verificacao(labirinto[x][y-1])) {
+            --y;
+            px.push(x);
+            py.push(y);
+            if (labirinto[x][y] != 'S') {
+                labirinto[x][y] = '*';
+                labirinto_mostra[x][y] = '*';
+            }
+            ++i;
+            ++i_;
+        }
+        else {
+            x = px.topo();
+            px.pop();
+            y = py.topo();
+            py.pop();
+            --i_;
+            labirinto_mostra[x][y] = ' ';
+        }
+    }
+
+    for (int i = 0; i < labirinto_mostra.size(); ++i) {
+        cout << labirinto_mostra[i] << endl;
+    }
+
+    cout << "Localizacao da S (Saida): " << "( " << px.topo() << ", " << py.topo() << " )" << endl << endl;
+    cout << "Quantidade de movimentos utilizados para concluir o labirinto: " << i << endl;
+
+    while (!px.vazia() && !py.vazia()) {
+        px_.push(px.topo());
+        px.pop();
+        py_.push(py.topo());
+        py.pop();
+    }
+
+    while (!px_.vazia() && !py_.vazia()) {
+        px_.pop();
+        py_.pop();
+        ++cont;
+    }
+}
+
+// -----------------------------------------------------------
+
+Pilha::Pilha() {  //Nao sao independentes, sao matodos pertencentes a classe 
+    topo_ = -1;
+}
+
+void Pilha::push(TipoDado elem) { //leva pro topo da pilha
+    if (!cheia()) {
+        ++topo_;
+        pilha[topo_] = elem;
+    }
+    else
+        cerr << "PILHA CHEIA!" << endl;
+}
+
+void Pilha::pop() {    //tira o elem do topo da pilha
+    if (!vazia()) {
+        --topo_;
+    }
+    else
+        cerr << "PILHA VAZIA!" << endl;
+}
+
+TipoDado Pilha::topo() {
+    if (!vazia())       // nao vazia?
+        return pilha[topo_];
+    else {
+        cerr << "PILHA VAZIA!" << endl;
+        return -1;
+    }
+}
+
+bool Pilha::vazia() {
+    return (topo_ == -1);
+
+    //if(topo_ == -1)
+    //    return true;
+    //else 
+    //    return false;
+}
+
+bool Pilha::cheia() {
+    return (topo_ == MAXPILHA - 1);
+
+    //if(topo_ == MAXPILHA -1)
+    //   return true;
+    //else
+    //    return false;
+}
